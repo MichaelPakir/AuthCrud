56fb32e45fbf1efce0f456445adc82e64f42c52c
MichaelPakir <neovimm@gmail.com>
2026-04-29 20:30:39 +0800

    Tweaks

--- src/pages/MyRecipes.jsx
+++ src/pages/MyRecipes.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate, useParams, Link } from 'react-router-dom'
 import { collection, query, where, getDocs, orderBy } from 'firebase/firestore'
 import { db } from '../lib/firebase'
 import { useAuth } from '../contexts/AuthContext'
